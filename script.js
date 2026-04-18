97b801d3b3ec986046b8cb3c3bd6b34fa734708b
shekharkhurkhuriya <khurkhuriyashekhar@gmail.com>
2026-04-19 00:38:14 +0530

    add js for clock

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ staff.forEach(function(card) {
         alert("Teacher: " + card.innerText);
     });
 });
+
+// clock 
+
+function updateClock() {
+    let now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    // AM / PM
+    let ampm = hours >= 12 ? "PM" : "AM";
+
+    // Convert 24h → 12h
+    hours = hours % 12;
+    hours = hours ? hours : 12; // 0 ko 12 banata hai
+
+    // Leading zero
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+
+    let time = hours + ":" + minutes + ":" + seconds + " " + ampm;
+
+    document.getElementById("clock").innerText = time;
+}
+
+// Run every second
+setInterval(updateClock, 1000);
+
+// Run instantly
+updateClock();
